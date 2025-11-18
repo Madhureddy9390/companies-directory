b11e15a0de70771ba9460c7f7782285b9213bc8a
Madhu Balu <madhureddy.balu@gmail.com>
2025-11-18 23:02:49 +0530

    Addedd logics

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,56 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import { MaterialReactTable } from "material-react-table";
 
 export default function App() {
   const [companies, setCompanies] = useState([]);
-
+  const [columns, setColumns] = useState([]);
   useEffect(() => {
-    axios.get("/companies.json")
-      .then((response) => {
-        setCompanies(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching companies:", error);
-      });
-  }, []);
+    fetchCompanies();
+  }, [])
+  async function fetchCompanies() {
+    try {
+      const res = await axios.get("/companies.json")
+      console.log(res.data)
+      setCompanies(res.data)
+
+      // Auto-generate columns from JSON keys
+      if (res.data.length > 0) {
+        const cols = Object.keys(res.data[0]).map((key) => ({
+          accessorKey: key,
+          header: key.charAt(0).toUpperCase() + key.slice(1),
+        }));
+
+        setColumns(cols);
+      }
+    }
+    catch (e) {
+      console.log("Error while fetching data from the API")
+      console.log(e)
+    }
+  }
+  // const columns = [
+  //   { accessorKey: "id", header : "ID"},
+  //   { accessorKey: "name", header: "Name" },
+  //   { accessorKey: "location", header: "Location" },
+  //   { accessorKey: "employees", header: "Employees" },
+  // ];
 
   return (
-    <div>
-      <h2>Company List</h2>
-      <h2>Just Checking</h2>
-
-      {companies.map((company) => (
-        <div key={company.id}>
-          <p><strong>Name:</strong> {company.name}</p>
-          <p><strong>Location:</strong> {company.location}</p>
-          <p><strong>Employees:</strong> {company.employees}</p>
-          <hr />
-        </div>
-      ))}
-    </div>
+    <Box
+    >
+      <Card >
+        <MaterialReactTable
+          columns={columns}
+          data={companies}
+          // enableColumnFilters ={false}
+          // enableSorting={false}
+          // enablePagination ={false}
+          // enableGlobalFilter ={false}
+        />
+      </Card>
+    </Box>
   );
 }
