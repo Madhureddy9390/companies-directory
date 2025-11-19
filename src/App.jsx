2617150b2a67b9299e72d53501d5923affc7a2ad
madhu.balu@rampgroup.com <madhu.balu@rampgroup.com>
2025-11-19 12:05:40 +0530

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -7,48 +7,55 @@ import { MaterialReactTable } from "material-react-table";
 export default function App() {
   const [companies, setCompanies] = useState([]);
   const [columns, setColumns] = useState([]);
+
   useEffect(() => {
     fetchCompanies();
-  }, [])
+  }, []);
+
   async function fetchCompanies() {
     try {
-      const res = await axios.get("/companies.json")
-      console.log(res.data)
-      setCompanies(res.data)
+      const res = await axios.get("/companies.json");
+      console.log(res.data);
+      setCompanies(res.data);
 
-      // Auto-generate columns from JSON keys
       if (res.data.length > 0) {
         const cols = Object.keys(res.data[0]).map((key) => ({
           accessorKey: key,
           header: key.charAt(0).toUpperCase() + key.slice(1),
         }));
-
         setColumns(cols);
       }
-    }
-    catch (e) {
-      console.log("Error while fetching data from the API")
-      console.log(e)
+    } catch (e) {
+      console.log("Error while fetching data from the API");
+      console.log(e);
     }
   }
-  // const columns = [
-  //   { accessorKey: "id", header : "ID"},
-  //   { accessorKey: "name", header: "Name" },
-  //   { accessorKey: "location", header: "Location" },
-  //   { accessorKey: "employees", header: "Employees" },
-  // ];
 
   return (
     <Box
+      sx={{
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }}
     >
-      <Card >
+      <Card
+        sx={{
+          border: "3px solid #1976d2",
+          borderRadius: 2,
+        }}
+      >
         <MaterialReactTable
           columns={columns}
           data={companies}
-          // enableColumnFilters ={false}
-          // enableSorting={false}
-          // enablePagination ={false}
+          enableFullScreenToggle={false}
+          // enableColumnFilters ={false} 
+          // enableSorting={false} 
+          // enablePagination ={false} 
           // enableGlobalFilter ={false}
+          initialState={{ pagination: { pageSize: 5 } }}
         />
       </Card>
     </Box>
